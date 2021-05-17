db08156c4c7a7d2ed2744074ae6064897a6187f7
NickCoooke <nickcoooke@gmail.com>
2021-05-17 15:13:59 -0700

    added Search Bar V1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './styles/App.css';
+
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <h2> Hello World</h2>
-   </div>
+      <header class='p-3 bg-dark text-white'>
+        <div className="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <h2> Fire Smoke Air: An Air Quality Dashboard</h2>
+            <Search />
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
 
