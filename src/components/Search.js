38c9814a7f1ba4ce5df16b7f9e4ea1b33c3ee6b8
NickCoooke <nickcoooke@gmail.com>
2021-05-26 22:03:07 -0700

    built mapbox component

--- src/components/Search.js
+++ src/components/Search.js
@@ -18,22 +18,4 @@ export default function Search() {
       </Button>
     </form>
   );
-//
-//    <div className="inline-block">
-//    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-//      <input type="search" 
-//            className="form-control form-control-dark" 
-//            placeholder="Search..." 
-//            aria-label="Search"/>
-//    </form>
-//    <div className="text-end">
-//      <button type="button" className="btn btn-outline-light me-2">Search</button>
-//    </div>
-//    </div>
-//  );
 }
-
-
-/* Sources
-  Search Bar tutorial:  https://www.emgoto.com/react-search-bar/
-*/
