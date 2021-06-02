fe2453903db5fe59e425d14246b0b4526a2bf72a
NickCoooke <nickcoooke@gmail.com>
2021-06-01 18:47:19 -0700

    reintroduced map.css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,81 @@
+import React, { Component } from 'react';
 
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './styles/App.css';
 
-
 import Map from './components/Map';
-//import TopBar from './components/TopBar';
-import Search from './components/Search';
-import SideBar from './components/SideBar'
+import TopBar from './components/TopBar';
+import InfoBox from './components/InfoBox';
+import AirNow from './adapters/AirNow';
 
+class App extends Component {
+state = {
+    data: null
+  };
 
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => {
+        console.log(res.express);
+        this.setState({data: res.express })
+      })
+      .catch(err => console.log(err));
+  }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
 
-export default function App() {
-  return (
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="App-title">Backend Connection: {this.state.data}</h1>
       <Container fluid>
         <Row>
-          <Col xs={2}>
-            home button
-          </Col>
-          <Col xs={8}>
-            <Search />
-            Search Bar 
-          </Col>
-          <Col xs={2}>
-            <SideBar />
-          </Col>
+        </Row>
+        <Row >
+          <TopBar />
         </Row>
       <Row>
         <Col>
-          <h2> MAP </h2>
           <Map />
         </Col>
       </Row>
+      <Row>
+        <Col>
+          <InfoBox />
+        </Col>
+      </Row>
       </Container>
-  );
+      </>
+    );
+  }
 }
+export default App;
+
+const loadAirNow = () => {
+  console.log(AirNow())
+  /*
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+    */
+}
+
+/* Sources
+How to handle secret Keys
+  https://hackernoon.com/how-to-use-environment-variables-keep-your-secret-keys-safe-secure-8b1a7877d69c
+
+*/
