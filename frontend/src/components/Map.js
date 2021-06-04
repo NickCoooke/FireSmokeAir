7a5802cdd37549f796b0f813a94697a0bb6967a4
NickCoooke <nickcoooke@gmail.com>
2021-06-04 14:34:31 -0700

    pre merge with main

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,31 +2,6 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import '../styles/Map.css'
 
-let geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-77.032, 38.913]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'Washington, D.C.'
-    }
-  },
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.414, 37.776]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'San Francisco, California'
-    }
-  }]
-};
 
 /******************MAP *************************/
 mapboxgl.accessToken =
