36f72195c96bb65528c683dc4aff96c383e2fc8c
NickCoooke <cooke@pdx.edu>
2021-12-23 10:03:51 -0800

    integrated responsive react-map-gl map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,41 +1,33 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import '../styles/Map.css'
-//import aqiToColor from '../resources/colors';
-//import AirNowData from '../resources/AirNowAPI_20212004162030.json'
-import Layer from '../adapters/mapboxLayer'
+import ReactMapGL, {Layer} from 'react-map-gl';
+import '../styles/Map.css';
+import * as aqiData from '../resources/AIRNOW_102020.json';
 
 
 /******************MAP *************************/
-mapboxgl.accessToken =
-    'pk.eyJ1IjoidG9hc3RlZGdyZWF0ZnJ1aXQiLCJhIjoiY2tueHVrc2RvMG9pbzJzbzg1d2QzbjJjbCJ9.WZZZmLABakfNWj1BLT7yEg'
 
 const Map = () => {
-  const mapContainerRef = useRef(null);
-
-  const [lng, setLng] = useState(-123);
-  const [lat, setLat] = useState(45);
-  const [zoom, setZoom] = useState(5);
-
-  // Initialize map when component mounts
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/dark-v10',
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
-  });
+ const [viewport, setViewport] = useState({
+   latitude: 45.5202,
+   longitude: -122.6742,
+   zoom: 10,
+   width: '100vw',
+   height: '70vh',
+ }) 
  return (
+   <ReactMapGL 
+    {...viewport}
+    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    onViewportChange={viewport => setViewport(viewport)}
+    mapStyle= "mapbox://styles/mapbox/dark-v10"
+
+   >
+     <Layer {...aqiData} id="aqiData102020" paint={{'fill-color': 'blue'}} />
+   </ReactMapGL>
+ );
+  
+ 
+ /*(
     <div>
       <div className='sidebarStyle'>
         <div>
@@ -44,7 +36,7 @@ const Map = () => {
       </div>
       <div className='map-container' ref={mapContainerRef} />
     </div>
-  );
+  );*/
 }
 
 export default Map;
@@ -52,6 +44,11 @@ export default Map;
 
 /*
 Good Tutorials
+  react-map-gl
+-   react-map-gl docs   = https://visgl.github.io/react-map-gl/docs
+-   intro tutorial vid  = https://www.youtube.com/watch?v=JJatzkPcmoI
+
+
 -   Load Large Datasets = https://docs.mapbox.com/help/troubleshooting/working-with-large-geojson-data/
 -   Using Layers on Mapbox= https://www.lostcreekdesigns.co/writing/a-complete-guide-to-sources-and-layers-in-react-and-mapbox-gl-js/
 -   Mapbox with react=  https://dev.to/laney/react-mapbox-beginner-tutorial-2e35
